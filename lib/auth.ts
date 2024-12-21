59c679649e8c66bf1c8d7bfe00063d4fce03be92
chairulnw <115411937+chairulnw@users.noreply.github.com>
2024-12-21 14:44:27 +0700

    secret

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,6 +14,7 @@ export const authOptions: NextAuthOptions = {
     jwt: {
       secret: process.env.JWT_SECRET,
     },
+    secret:process.env.NEXTAUTH_SECRET,
     pages:{
         signIn: '/login'
     },
